0afe6f68bd2d80dfd47bf8ada90a9e75c0620565
zengshan233 <zengs8816@163.com>
2023-03-26 11:57:21 +0800

    s

--- dist/esm/components/MsgItem/index.js
+++ dist/esm/components/MsgItem/index.js
@@ -1,33 +1,60 @@
-import React from 'react';
-import style from "./style.module.css";
-import MsgBubble from "../MsgBubble";
-import dayjs from 'dayjs';
+import React from 'react'
+import style from './style.module.css'
+import MsgBubble from '../MsgBubble'
+import dayjs from 'dayjs'
 export default function MsgItem(_ref) {
   var data = _ref.data,
-    me = _ref.me;
-  var isMe = data.user.id === me.id;
-  return /*#__PURE__*/React.createElement("div", {
-    className: style.content,
-    style: {
-      flexDirection: isMe ? 'row-reverse' : 'row'
-    }
-  }, /*#__PURE__*/React.createElement("div", {
-    className: style.avatar
-  }, /*#__PURE__*/React.createElement("img", {
-    src: data.user.avatar
-  })), /*#__PURE__*/React.createElement("div", {
-    className: style.text_area,
-    style: {
-      alignItems: isMe ? 'flex-end' : 'flex-start'
-    }
-  }, /*#__PURE__*/React.createElement("div", {
-    className: style.comment_area
-  }, /*#__PURE__*/React.createElement("span", {
-    className: style.nickname_text
-  }, data.user.nickname), /*#__PURE__*/React.createElement("span", {
-    className: style.date_text
-  }, dayjs.unix(data.date).format('MM-DD HH:mm:ss'))), /*#__PURE__*/React.createElement(MsgBubble, {
-    isMe: isMe,
-    data: data.message
-  })));
+    me = _ref.me
+  var isMe = data.user.id === me.id
+  return /*#__PURE__*/ React.createElement(
+    'div',
+    {
+      className: style.content,
+      style: {
+        flexDirection: isMe ? 'row-reverse' : 'row',
+      },
+    },
+    /*#__PURE__*/ React.createElement(
+      'div',
+      {
+        className: style.avatar,
+      },
+      /*#__PURE__*/ React.createElement('img', {
+        src: data.user.avatar,
+      }),
+    ),
+    /*#__PURE__*/ React.createElement(
+      'div',
+      {
+        className: style.text_area,
+        style: {
+          alignItems: isMe ? 'flex-end' : 'flex-start',
+        },
+      },
+      /*#__PURE__*/ React.createElement(
+        'div',
+        {
+          className: style.comment_area,
+        },
+        /*#__PURE__*/ React.createElement(
+          'span',
+          {
+            className: style.nickname_text,
+          },
+          data.user.nickname,
+        ),
+        /*#__PURE__*/ React.createElement(
+          'span',
+          {
+            className: style.date_text,
+          },
+          dayjs.unix(data.date).format('MM/DD HH:mm'),
+        ),
+      ),
+      /*#__PURE__*/ React.createElement(MsgBubble, {
+        isMe: isMe,
+        data: data.message,
+      }),
+    ),
+  )
 }
--- src/components/MsgItem/index.tsx
+++ src/components/MsgItem/index.tsx
@@ -8,19 +8,17 @@ export default function MsgItem({ data, me }: IMsgItem) {
   const isMe = data.user.id === me.id
 
   return (
-    <div
-      className={style.content}
-      style={{ flexDirection: isMe ? 'row-reverse' : 'row' }}>
+    <div className={style.content} style={{ flexDirection: isMe ? 'row-reverse' : 'row' }}>
       <div className={style.avatar}>
         <img src={data.user.avatar} />
       </div>
       <div className={style.text_area} style={{ alignItems: isMe ? 'flex-end' : 'flex-start' }}>
         <div className={style.comment_area}>
           <span className={style.nickname_text}>{data.user.nickname}</span>
-          <span className={style.date_text}>{dayjs.unix(data.date).format('MM-DD HH:mm:ss')}</span>
+          <span className={style.date_text}>{dayjs.unix(data.date).format('MM/DD HH:mm')}</span>
         </div>
         <MsgBubble isMe={isMe} data={data.message} />
       </div>
     </div>
-    )
+  )
 }
